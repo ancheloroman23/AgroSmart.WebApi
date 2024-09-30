d281347cb820caf42a9d9b7eef7dd97f52a07a9b
ancheloroman23 <112509415+ancheloroman23@users.noreply.github.com>
2024-09-30 17:00:27 -0400

    GenericRepository Added.

---
+++ AgroSmart.Infrastructure.Persistence/Context/ApplicationContext.cs
@@ -0,0 +1,31 @@
+﻿using AgroSmart.Core.Domain.Commons;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgroSmart.Infraestructure.Persistence.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.Created = DateTime.Now;
+                        entry.Entity.CreatedBy = "DefaultAppUser";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModified = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "DefaultAppUser";
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+
+    }
+}
